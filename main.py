7edb3791a4003417673718c8132deb912c89ad2d
RFerMartinez <ricardofernandomartinez0@gmail.com>
2026-06-09 08:58:45 -0300

    punto 1.1

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+print("✅ Entorno configurado correctamente:")
+print(f" -> NumPy: {np.__version__}")
+print(f" -> Pandas: {pd.__version__}")
+print(f" -> Scikit-Learn: {sklearn.__version__}")
